f3c3256c76fb49ffacd05874112c51f737d794f0
Dhananjay Tulaskar <drtulaskar98@gmail.com>
2026-04-29 23:20:11 +0530

    add some more functionality of responsiveness

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
-const icon = document.querySelector(".icon");
-const showContent = document.querySelector(".content_container");
+const hamburger_icon = document.querySelector("#hamburger_icon");
+const hamburger_content = document.querySelector("#hamburger_content");
 
-icon.addEventListener("click", (e) => {
-    icon.classList.toggle("cross");
-    showContent.classList.toggle("show_content");
-    document.querySelector("body").classList.toggle("activate")
+hamburger_icon.addEventListener("click", (e) => {
+    hamburger_icon.classList.toggle("cross_icon");
+    hamburger_content.classList.toggle("activate_menu");
+    // console.log(e.currentTarget.id);
+    if(e.currentTarget.id === "hamburger_icon"){   
+        if(e.currentTarget.className === "cross_icon"){
+            console.log("Menu open");
+            document.querySelector("body").classList.add("open-menu");
+        }else{
+            console.log("Menu close");
+            document.querySelector("body").classList.remove("open-menu");
+        }
+    }
 })
 
 console.log("Clicked");
